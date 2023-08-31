7b0db529ffa34731b429d6b0370f64e92161f8f1
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-09-01 00:09:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.9.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.9.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/654400.webp" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Inter+Tight:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter+Tight:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  <script>
+		!function (w, d, t) {
+		  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++
+)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+		
+		  ttq.load(\'CJO7PS3C77U5EBTKVE6G\');
+		  ttq.page();
+		}(window, document, \'ttq\');
+	</script>
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="header13 cid-tNSIMlMbMu" id="header13-2">
+  
+
+  
+  
+
+  <div class="align-center container">
+    <div class="row justify-content-center">
+      <div class="col-md-12 col-lg-9">
+        <h1 class="mbr-section-title mbr-fonts-style mb-4 display-2"><strong><em>Time To Take Advantage From <u>Your Beautiful Photos</u>!!</em></strong></h1>
+        
+        
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-4 mt-4">
+        <a href="https://hop.clickbank.net/?affiliate=unempl&vendor=socialpaid&pid=videojobquiz495"><img src="assets/images/story.image.text.2.2-750x1334.jpg" alt="Mobirise Website Builder" title=""></a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section data-bs-version="5.1" class="info3 cid-tNSjaZLigw" id="info3-1">
+
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-center">
+                        
+                        
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-primary display-4" href="https://7630bfo9vjxzdy59ndpaocn1c1.hop.clickbank.net/?tid=ttads">Click Here To Watch The Video</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/ytplayer/index.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  
+</body>
+</html>
+';
+?>
